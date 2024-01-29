12a1d297caca971b7a78759a607c15eb2b7c18f5
StephDietz <steph.dietz@vercel.com>
2024-01-29 17:35:47 -0600

    styles

--- index.js
+++ index.js
@@ -15,19 +15,15 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/' + 'home.htm');
 });
 
-app.get('/get_form', function (req, res) {
-	res.sendFile(__dirname + '/' + 'get_form.htm');
+app.get('/about', function (req, res) {
+	res.sendFile(__dirname + '/' + 'about.htm');
 });
 
-app.get('/post_form', function (req, res) {
-	res.sendFile(__dirname + '/' + 'post_form.htm');
-});
-
-app.get('/user_upload_form', function (req, res) {
+app.get('/uploadUser', function (req, res) {
 	res.sendFile(__dirname + '/' + 'user_upload_form.htm');
 });
 
-app.post('/addUser', urlencodedParser, async (req, res) => {
+app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
 	try {
 		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
 		res.status(200).send('<h1>User added successfully</h1>');
@@ -37,18 +33,77 @@ app.post('/addUser', urlencodedParser, async (req, res) => {
 	}
 });
 
-app.get('/getUsers', async (req, res) => {
+app.get('/allUsers', async (req, res) => {
 	try {
 		const users = await sql`SELECT * FROM Users;`;
 		console.log(users.rows);
-		if (users) {
-			res.status(200).send(`<h1>Users</h1><pre>${JSON.stringify(users.rows, null, 2)}</pre>`);
+		if (users && users.rows.length > 0) {
+			let tableContent = users.rows
+				.map(
+					(user) =>
+						`<tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                </tr>`
+				)
+				.join('');
+
+			res.status(200).send(`
+                <html>
+                    <head>
+                        <title>Users</title>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                            }
+                            table {
+                                width: 100%;
+                                border-collapse: collapse;
+																margin-bottom: 15px;
+                            }
+                            th, td {
+                                border: 1px solid #ddd;
+                                padding: 8px;
+                                text-align: left;
+                            }
+                            th {
+                                background-color: #f2f2f2;
+                            }
+														a {
+															text-decoration: none;
+															color: #0a16f7;
+															margin: 15px;
+														}
+                        </style>
+                    </head>
+                    <body>
+                        <h1>Users</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>User ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                ${tableContent}
+                            </tbody>
+                        </table>
+												<div>
+													<a href="/">Home</a>
+													<a href="/uploadUser">Add User</a>
+												</div>
+                    </body>
+                </html>
+            `);
 		} else {
 			res.status(404).send('Users not found');
 		}
 	} catch (error) {
 		console.error(error);
-		res.status(500).send('Error retrieving user');
+		res.status(500).send('Error retrieving users');
 	}
 });
 
